814fdd7b7cc2ca64df9a8f9785151387c2a4f193
Hamza Qadir <aameerhamzahamza1@gmail.com>
2025-10-16 18:51:57 +0500

    fix: correct schema definition for user model by ensuring unique and lowercase constraints for username and email

---
+++ models/user.models.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+const userschema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+    }
+},{timestamps:true})
+export const User = mongoose.model("User",userschema)
