9f1809bb0c249f85fb2ae1b7cb4047aac7c54f01
Hamza Qadir <aameerhamzahamza1@gmail.com>
2025-10-18 08:09:48 +0500

    update product and category code

--- models/user.models.js
+++ models/user.models.js
@@ -3,6 +3,19 @@ const userschema = new mongoose.Schema({
     username:{
         type: String,
         required: true,
+        unique : true,
+        lowercase: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique : true,
+        lowercase: true
+    },
+    password:{
+        type: String,
+        required: true,
     }
+
 },{timestamps:true})
 export const User = mongoose.model("User",userschema)
